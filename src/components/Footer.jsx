d5f589262fe90ea7a380f708d77b201ed5fb9608
rahultaakhr <rahultaakhr@gmail.com>
2025-01-12 17:51:07 +0530

    add github link

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -7,7 +7,8 @@ function Footer() {
                 <ul className='  flex gap-6 items-center justify-center text-xl md:text-4xl py-2 font-bold'>
                     <li className=' bg-clip-text text-transparent bg-gradient-to-r from-pink-400 via-pink-500 to-pink-700'><a href="https://www.instagram.com/lifelike.code" target='_blank'><i className="fa-brands fa-instagram"></i></a></li>
                     <li className=' bg-clip-text text-transparent bg-gradient-to-r from-gray-800 via-gray-900 to-gray-700'><a href="https://www.x.com/rahultaakhr" target='_blank'><i className="fa-brands fa-x-twitter"></i></a></li>
-                    <li className=' bg-clip-text text-transparent bg-gradient-to-r from-blue-800 via-blue-700 to-blue-900'><a href="https://www.linkedin.com/in/rahul-taak-hr" target='_blank'><i className="fa-brands fa-linkedin"></i></a></li>
+                    <li className=' bg-clip-text text-transparent bg-gradient-to-tr from-blue-400 via-blue-500 to-blue-500'><a href="https://www.linkedin.com/in/rahul-taak-hr" target='_blank'><i className="fa-brands fa-linkedin"></i></a></li>
+                    <li className=' bg-clip-text text-transparent bg-gradient-to-br  from-gray-800 via-gray-900 to-gray-950'><a href="https://www.github.com/rahultaakhr" target='_blank'><i class="fa-brands fa-github"></i></a></li>
                 </ul>
                 <p>Design By Rahul  &#169; 2025</p>
             </div>
